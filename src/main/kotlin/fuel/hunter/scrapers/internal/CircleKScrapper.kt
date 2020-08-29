1b4972a6d88e958fc187954749c7136739ed8b90
sashjakk <sashjakk@gmail.com>
2020-08-30 00:47:35 +0300

    Redo Circle K - homepage updated

--- src/main/kotlin/fuel/hunter/scrapers/internal/CircleKScrapper.kt
+++ src/main/kotlin/fuel/hunter/scrapers/internal/CircleKScrapper.kt
@@ -1,77 +1,59 @@
 package fuel.hunter.scrapers.internal
 
-import fuel.hunter.models.Price
 import fuel.hunter.extensions.price
+import fuel.hunter.models.Price
+import fuel.hunter.repo.Repository
+import fuel.hunter.tools.toAddressRegex
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-
-private const val providerName = "Circle K"
-
-private val priceRegex = "(.+)\\s(.+)".toRegex()
 
 private val fuelTypeMap = mapOf(
-    "1335453340249" to Price.FuelType.E95,
-    "1335520335162" to Price.FuelType.E95,
-    "1335453340279" to Price.FuelType.DD,
-    "1335520335169" to Price.FuelType.DD,
-    "1335453340203" to Price.FuelType.GAS
+    "miles 95" to Price.FuelType.E95,
+    "milesPLUS 98" to Price.FuelType.E98,
+    "miles D" to Price.FuelType.DD,
+    "milesPLUS D" to Price.FuelType.DD,
+    "Autogāze" to Price.FuelType.GAS
 )
 
-class CircleKScrapper : Scraper {
+class CircleKScrapper(private val repo: Repository) : Scraper {
     override fun scrape(document: Document): Flow<Price> = flow {
-        document.circlekSnapshotChunks.forEach { chunk ->
-            val fuelType = getFuelType(chunk[0].html())
+        val stations = repo.getStations()
 
-            val (rawPrice) = priceRegex
-                .matchEntire(chunk[1].text())
-                ?.destructured
-                ?: throw IllegalArgumentException("Invalid price")
+        val chunks = document
+            .select("table tr")
+            .drop(1)
+            .map { it.children().map { it.text() } }
 
-            chunk[2].text()
-                .split(", ")
-                .forEach { raw ->
-                    val composedName = raw.streetName
-                        .takeUnless { it.isEmpty() }
-                        ?.let { "$providerName, $it" }
-                        ?: providerName
+        chunks
+            .map { (rawType, rawPrice, rawAddress) ->
+                val regex = rawAddress.toAddressRegex()
 
-                    val item = price {
-                        name = composedName
-                        address = raw
+                val station = stations
+                    .find { s -> s.address.matches(regex) }
+
+                val price = station
+                    ?.let {
+                        price {
+                            name = station.name
+                            address = station.address
+                            city = station.city
+                            stationId = station.id
+                            type = fuelTypeMap[rawType]
+                            price = rawPrice.replace(" EUR", "").toFloat()
+                        }
+                    }
+                    ?: price {
+                        name = "Circle K, $rawAddress"
+                        address = rawAddress
                         city = "Rīga"
-                        type = fuelType
+                        stationId = ""
+                        type = fuelTypeMap[rawType]
                         price = rawPrice.toFloat()
                     }
 
-                    emit(item)
-                }
-        }
-    }
-
-    private fun getFuelType(rawImageTag: String): Price.FuelType {
-        val key = fuelTypeMap.keys.firstOrNull { rawImageTag.contains(it) }
-            ?: throw IllegalArgumentException("Unknown type provided")
-
-        return fuelTypeMap[key]
-            ?: throw IllegalArgumentException("Type not registered in map")
+                emit(price)
+            }
     }
 }
 
-private val String.streetName: String
-    get() {
-        return indexOf("iela")
-            .takeUnless { it == -1 }
-            ?.let { substring(0, it - 1) }
-            ?: ""
-    }
-
-private val Document.circlekSnapshotChunks: List<List<Element>>
-    get() {
-        return this
-            .select("table.fuelprices tr")
-            .drop(1)
-            .flatMap { it.children() }
-            .windowed(3, 3)
-    }
